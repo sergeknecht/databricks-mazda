6d7ca317f856ee86465ee75da7ff47405ec7e04e
serge.knecht <serge.knecht@gmail.com>
2023-11-25 15:09:37 +0100

    nice formatted text

--- _00_clusters_list.py
+++ _00_clusters_list.py
@@ -2,6 +2,7 @@ from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.compute import State
 
 w = WorkspaceClient()
+format_str = "{0: >25}"
 
 for c in w.clusters.list():
     if c.state == State.TERMINATED:
@@ -9,20 +10,20 @@ for c in w.clusters.list():
         # w.clusters.delete(c.cluster_id)
         continue
 
-    print(c.state, c.state_message)
+    print(format_str.format("state:"), c.state, c.state_message)
 
-    print(c.cluster_name)
-    print(c.cluster_id)
-    print(c.spark_context_id)
-    print(c.spark_version)
-    print(c.node_type_id)
-    print(c.driver_node_type_id)
-    print(c.autoscale)
-    print(c.num_workers)
-    print(c.cluster_source)
-    print(c.start_time)
-    print(c.terminated_time)
-    print(c.last_state_loss_time)
-    print(c.spark_env_vars)
-    print(c.spark_conf)
+    print(format_str.format("cluster_name:"), c.cluster_name)
+    print(format_str.format("stacluster_idte:"), c.cluster_id)
+    print(format_str.format("spark_context_id:"), c.spark_context_id)
+    print(format_str.format("spark_version:"), c.spark_version)
+    print(format_str.format("node_type_id:"), c.node_type_id)
+    print(format_str.format("driver_node_type_id:"), c.driver_node_type_id)
+    print(format_str.format("autoscale:"), c.autoscale)
+    print(format_str.format("num_workers:"), c.num_workers)
+    print(format_str.format("cluster_source:"), c.cluster_source)
+    print(format_str.format("start_time:"), c.start_time)
+    print(format_str.format("terminated_time:"), c.terminated_time)
+    print(format_str.format("last_state_loss_time:"), c.last_state_loss_time)
+    print(format_str.format("spark_env_vars:"), c.spark_env_vars)
+    print(format_str.format("spark_conf:"), c.spark_conf)
     print("-" * 80)
